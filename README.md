87892a77482131f4a88b32ae1ad1cb3135de758d
Spooghetti420 <kkovachev1947@gmail.com>
2022-07-18 19:36:08 +0100

    Add project

---
+++ README.md
@@ -0,0 +1,11 @@
+# Colors
+A module for printing colored text in Python. The module is a basic wrapper for
+the [ANSI color escape codes](https://en.wikipedia.org/wiki/ANSI_escape_code#Colors) that
+some (many modern) terminals use to provide color control for text passages.
+Using the `printc` function, with a string to format, and a number of color or style constants,
+e.g. `RED`, `BOLD`, or `UNDERLINE`, you can output a colored string! Check out the main function in the module for an example of usage.
+
+## Screenshot
+![A number of colored passages, showing many different foreground and background color combinations](image/preview.png "Color output")
+
+Forgot to include any underlined text, but the `UNDERLINE` setting can be used in place of or in combination with the `BOLD` modifier.
---
+++ colors.py
@@ -0,0 +1,80 @@
+"""
+This moudle is made to encode xterm-256color escape sequences as convenient
+variables and functions for use in a command-line utility.
+"""
+
+from typing import Callable, Sequence
+
+BLACK = "30"
+RED = "31"
+GREEN = "32"
+YELLOW = "33"
+BLUE = "34"
+MAGENTA = "35"
+CYAN = "36"
+WHITE = "37"
+
+BLACK_BG = "40"
+RED_BG = "41"
+GREEN_BG = "42"
+YELLOW_BG = "43"
+BLUE_BG = "44"
+MAGENTA_BG = "45"
+CYAN_BG = "46"
+WHITE_BG = "47"
+
+RESET = "0"
+BOLD = "1"
+UNDERLINE = "4"
+INVERSE = "7"
+BOLD_OFF = "21"
+UNDERLINE_OFF = "24"
+INVERSE_OFF = "27"
+
+
+def wrap_string(s: str, start_wrapping: str, end_wrapping=None) -> str:
+    """
+    Surrounds the input string `s` in the input wrapping.
+    If the string is to be wrapped in two separate wrappings,
+    one start and one end, then the `end_wrapping` can be specified
+    separately. Else, if the end wrapping is not specified, it
+    defaults to the start wrapping.
+    """
+    return f"{start_wrapping}{s}{end_wrapping if end_wrapping is not None else start_wrapping}"
+
+
+def wrap_colors(s: str, *colors: str) -> str:
+    """
+    Returns a string corresponding to the escape sequence for a
+    number of terminal colors, followed by the escape sequence to reset the color.  
+    """
+    return wrap_string(s, f'\033[{";".join(colors)}m', f'\033[{RESET}m')
+
+
+def printc(s: str, *formats, **kwargs):
+    """
+    Prints a string with a list of formatting options.
+    Use `kwargs` for options to the `print` function.
+    """
+    print(wrap_colors(s, *formats), **kwargs)
+
+
+def main():
+    printc("==Colors==", BOLD, WHITE)
+    printc("Beautiful, pure white text", WHITE)
+    printc("Red bold text", BOLD, RED)
+    printc("Yellow bold text", BOLD, YELLOW)
+    printc("Green bold text", BOLD, GREEN)
+    printc("Blue bold text", BOLD, BLUE)
+    print("Unaffected remainder text")
+
+    printc("Black text on white background", BLACK, WHITE_BG)
+    print()
+    printc(" "*10, RED_BG)
+    printc(" Rainbow! ", BLACK, BOLD, YELLOW_BG)
+    printc(" "*10, GREEN_BG)
+    printc(" "*10, BLUE_BG)
+
+
+if __name__ == "__main__":
+    main()
